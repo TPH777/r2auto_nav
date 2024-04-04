e092055f3dfbe5b3dc2f4cd849b522734470b1ea
TPH777 <pinghuitan80@gmail.com>
2024-04-04 22:13:18 +0800

    V3.1 Forward moving calibrated

--- test.py
+++ test.py
@@ -9,21 +9,25 @@ import requests
 GPIO.setmode(GPIO.BCM)
 leftIr = 2 # Left IR GPIO Pin
 rightIr = 3 # Right IR GPIO Pin
+lside = 15
+rside = 21
 GPIO.setup(leftIr, GPIO.IN)
 GPIO.setup(rightIr, GPIO.IN)
+GPIO.setup(lside, GPIO.IN)
+GPIO.setup(rside, GPIO.IN)
 
-# Servo setup
+# Servo setup to drop
 servoPin = 12
 GPIO.setup(servoPin, GPIO.OUT)
 servo = GPIO.PWM(servoPin, 50)
 
 # constants
 maxSpeed = 0.1
-reducedSpeed = 0.0
-nudgeAngle = 2.3
-turnSpeed = 1.0
+reducedSpeed = 0.01
+nudgeAngle = 0.5
+turnSpeed = 0.05
 turnAngle = 1.0 # +ve is left, -ve is right 
-turnTime = 1.8
+turnTime = 1.6
 ip_address = "192.168.18.191"
 
 class Mission(Node):
@@ -52,8 +56,8 @@ class Mission(Node):
         except requests.exceptions.RequestException as e:
             print('HTTP Request failed', e)
             
-    def shoot(self):
-        print("in shoot")
+    def drop(self):
+        print("in drop")
         servo.start(0)
         servo.ChangeDutyCycle(7.5) # Extend servo
         time.sleep(1)
@@ -64,15 +68,17 @@ class Mission(Node):
         twist = Twist()
         twist.linear.x = 0.0
         twist.angular.z = 0.0
+        time.sleep(0.1)
         self.publisher_.publish(twist)
         self.juncCount += 1 # Junction encountered +1
         print(self.juncCount)
+        time.sleep(2)
 # =============================================================================
 #         if (self.juncCount == 1):
 #             self.httpCall()
 #             self.get_logger().info('http')
 #         elif (self.juncCount == 2):
-#             self.shoot()
+#             self.drop()
 #             self.get_logger().info('shoot')
 #         else: # juncCount = 3
 #             #self.autoNav()
@@ -81,7 +87,7 @@ class Mission(Node):
 #         self.get_logger().info('stop')
 # =============================================================================
 
-    def turnRight(self):
+    def turnLeft(self):
         twist = Twist()
         twist.linear.x = turnSpeed
         twist.angular.z = turnAngle
@@ -89,12 +95,12 @@ class Mission(Node):
         self.publisher_.publish(twist)
         time.sleep(turnTime)
         
-    def turnLeft(self):
+    def turnRight(self):
         twist = Twist()
         twist.linear.x = turnSpeed
         twist.angular.z = -turnAngle
-        self.publisher_.publish(twist)
         time.sleep(0.1)
+        self.publisher_.publish(twist)
         time.sleep(turnTime)
         
     def nudgeLeft(self):
@@ -119,24 +125,30 @@ class Mission(Node):
         self.get_logger().info('forward')   
         
     def irmover(self):
-        leftOnLine = GPIO.input(leftIr) 
-        rightOnLine = GPIO.input(rightIr)
+        leftOut = GPIO.input(leftIr) 
+        rightOut = GPIO.input(rightIr)
+        lOnLine = GPIO.input(lside) 
+        rOnLine = GPIO.input(rside) 
 
-        if (leftOnLine): # 1 if on line, 0 if not on line
-              if(rightOnLine):
-                  self.stopBot()
-              else:
-                  self.nudgeLeft()
+        if lOnLine and rOnLine:
+            self.stopBot()
+        elif rOnLine:
+            self.turnRight()
+            time.sleep(turnTime)
+        elif lOnLine:
+            self.turnLeft()
+            time.sleep(turnTime)
+        elif not leftOut:
+            self.nudgeRight()
+        elif not rightOut:
+            self.nudgeLeft()
         else:
-              if (rightOnLine):
-                  self.nudgeRight()
-              else:
-                  self.forward()
-                   
+            self.forward()
+
     def mover(self):
         try:
             while True:
-                #self.shoot()
+                #self.drop()
                 self.irmover()
         except Exception as e:
             print(e)
