089ffc28c1af04136f2da19a839a6f8066a162c0
TPH777 <pinghuitan80@gmail.com>
2024-04-06 13:28:17 +0800

    V3.2 Change movement direction for stability

--- test.py
+++ test.py
@@ -7,14 +7,10 @@ import requests
 
 # IR Setup
 GPIO.setmode(GPIO.BCM)
-leftIr = 2 # Left IR GPIO Pin
-rightIr = 3 # Right IR GPIO Pin
-lside = 15
-rside = 21
+leftIr = 15 # Left IR GPIO Pin
+rightIr = 2 # Right IR GPIO Pin
 GPIO.setup(leftIr, GPIO.IN)
 GPIO.setup(rightIr, GPIO.IN)
-GPIO.setup(lside, GPIO.IN)
-GPIO.setup(rside, GPIO.IN)
 
 # Servo setup to drop
 servoPin = 12
@@ -22,11 +18,11 @@ GPIO.setup(servoPin, GPIO.OUT)
 servo = GPIO.PWM(servoPin, 50)
 
 # constants
-maxSpeed = 0.1
-reducedSpeed = 0.01
-nudgeAngle = 0.5
-turnSpeed = 0.05
-turnAngle = 1.0 # +ve is left, -ve is right 
+maxSpeed = -0.1
+reducedSpeed = -0.01
+nudgeAngle = -0.5
+turnSpeed = -0.05
+turnAngle = -1.0 # +ve is left, -ve is right 
 turnTime = 1.6
 ip_address = "192.168.18.191"
 
@@ -60,7 +56,7 @@ class Mission(Node):
         print("in drop")
         servo.start(0)
         servo.ChangeDutyCycle(7.5) # Extend servo
-        time.sleep(1)
+        time.sleep(2)
         servo.stop()
         GPIO.cleanup()
     
@@ -91,17 +87,17 @@ class Mission(Node):
         twist = Twist()
         twist.linear.x = turnSpeed
         twist.angular.z = turnAngle
-        time.sleep(0.1)
+        time.sleep(0.2)
         self.publisher_.publish(twist)
-        time.sleep(turnTime)
+        #time.sleep(turnTime)
         
     def turnRight(self):
         twist = Twist()
         twist.linear.x = turnSpeed
         twist.angular.z = -turnAngle
-        time.sleep(0.1)
+        time.sleep(0.2)
         self.publisher_.publish(twist)
-        time.sleep(turnTime)
+        #time.sleep(turnTime)
         
     def nudgeLeft(self):
         twist = Twist()
@@ -125,25 +121,19 @@ class Mission(Node):
         self.get_logger().info('forward')   
         
     def irmover(self):
-        leftOut = GPIO.input(leftIr) 
-        rightOut = GPIO.input(rightIr)
-        lOnLine = GPIO.input(lside) 
-        rOnLine = GPIO.input(rside) 
-
-        if lOnLine and rOnLine:
-            self.stopBot()
-        elif rOnLine:
-            self.turnRight()
-            time.sleep(turnTime)
-        elif lOnLine:
-            self.turnLeft()
-            time.sleep(turnTime)
-        elif not leftOut:
-            self.nudgeRight()
-        elif not rightOut:
-            self.nudgeLeft()
+        leftOnLine = GPIO.input(leftIr) 
+        rightOnLine = GPIO.input(rightIr)
+        
+        if leftOnLine:
+            if rightOnLine:
+                self.stopBot()
+            else:
+                self.nudgeLeft()
         else:
-            self.forward()
+            if rightOnLine:
+                self.nudgeRight()
+            else:
+                self.forward() 
 
     def mover(self):
         try:
